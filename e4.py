4c738c99e1bccc6346c971731e671b497369e65b
phyuthinzarmg <phyuthinzarmaungcu@gmail.com>
2018-06-07 10:55:10 +0630

    my first python ex

---
+++ e4.py
@@ -0,0 +1,7 @@
+print("There are ", cars, "cars  available.")
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool-capacity, "people today.")
+print("We have", passenger "to carpool today.")
+print("We need to put about", average_passengers_per, "in each car."
+        )
